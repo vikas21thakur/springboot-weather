77fad8d0983f9013d9669ef19e5c744616e3121b
vikas21thakur <vikas21thakur@gmail.com>
2022-04-09 12:18:34 +0800

    First commit

---
+++ src/main/java/com/weather/report/springbootweather/SpringbootWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.weather.report.springbootweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/report/springbootweather/SpringbootWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.report.springbootweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
